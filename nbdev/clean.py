cb13dea1af39d2dfa47ca3242855cd1bbe7a1a73
Matthias Griebel <matthias.griebel@uni-wuerzburg.de>
2020-09-08 13:18:24 +0000

    adding colab "id" cell tag

--- nbdev/clean.py
+++ nbdev/clean.py
@@ -33,7 +33,7 @@ def clean_cell_output(cell):
             clean_output_data_vnd(o)
 
 # Cell
-cell_metadata_keep = ["hide_input", "tags", "colab_type", "cellView"]
+cell_metadata_keep = ["hide_input", "tags", "colab_type", "cellView", "id"]
 nb_metadata_keep   = ["kernelspec", "jekyll", "jupytext", "doc"]
 
 # Cell
